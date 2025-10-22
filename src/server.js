40faff5385af528a7178ed81113a26c2c45164f0
Yunus Emre Manav <yunusemremanav@gmail.com>
2025-10-22 20:56:23 +0300

    first change for something

--- src/server.js
+++ src/server.js
@@ -5,14 +5,19 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
+// some change
 app.use(cors());
 app.use(express.json());
 
 app.get("/api", (req, res) => {
     res.send("Hello world!");
 });
+
+app.put("/api", (req, res) => {
+    console.log("put req called");
+})
 
 app.listen(PORT, () => {
     console.log(`Server listens on port ${PORT}`);
