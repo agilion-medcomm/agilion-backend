245062d1c379c2e1d28c80a96debe6ae8d8f3415
Linaruu <uagceng@gmail.com>
2025-12-10 22:44:06 +0300

    photoUrl fix

--- src/services/auth/profile.service.js
+++ src/services/auth/profile.service.js
@@ -21,6 +21,7 @@ const getUserProfile = async (userId) => {
             phoneNumber: true,
             dateOfBirth: true,
             role: true,
+            profilePhoto: true,
             doctor: {
                 select: {
                     id: true,
@@ -28,6 +29,11 @@ const getUserProfile = async (userId) => {
                 },
             },
             admin: {
+                select: {
+                    id: true,
+                },
+            },
+            cleaner: {
                 select: {
                     id: true,
                 },
@@ -62,6 +68,7 @@ const getUserProfile = async (userId) => {
         email: user.email,
         phoneNumber: user.phoneNumber,
         dateOfBirth: user.dateOfBirth,
+        photoUrl: user.profilePhoto,
         role: user.role,
     };
 
